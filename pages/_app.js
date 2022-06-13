f09b0d32cf58db7e9d3f278f580a38bff59f1dd1
devansh <devanshkhullar11@gmail.com>
2022-06-14 01:51:06 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Filters from "../components/Filters";
+import Navbar from "../components/Navbar";
+import "../styles/index.css";
+import {useState} from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [darkMode,setDarkMode]=useState(false);
+
+
+  return<>
+  <Navbar darkMode={darkMode} setDarkMode={setDarkMode}  />
+  <Filters darkMode={darkMode}  />
+  <Component darkMode={darkMode} {...pageProps} />
+  </> 
 }
 
 export default MyApp
